c611a9e1c70f8330147f86819995b73098e065b7
Whizfarsh <oyefarsh@gmail.com>
2023-09-07 23:31:33 +0100

    made changes to the background image

--- script.js
+++ script.js
@@ -18,22 +18,22 @@ let startX;
 let scrollLeft;
 const draggableElement = document.querySelector('.SC-hourly-details');
 
-console.log(startX, scrollLeft);
-console.log(`ofS1: ${draggableElement.offsetLeft}`);
+// console.log(startX, scrollLeft);
+// console.log(`ofS1: ${draggableElement.offsetLeft}`);
 draggableElement.addEventListener('touchstart', e => {
-  console.log(e);
-  console.log(`T1: ${e.touches[0].pageX}`);
+  // console.log(e);
+  // console.log(`T1: ${e.touches[0].pageX}`);
   startX = e.touches[0].pageX - draggableElement.offsetLeft;
-  console.log(`StartX2: ${startX}`);
-  console.log(draggableElement.scrollLeft);
+  // console.log(`StartX2: ${startX}`);
+  // console.log(draggableElement.scrollLeft);
   //   console.log(draggableElement.offsetLeft);
   scrollLeft = draggableElement.scrollLeft;
-  console.log(`S2: ${scrollLeft}`);
+  // console.log(`S2: ${scrollLeft}`);
 });
 
 draggableElement.addEventListener('touchmove', e => {
-  console.log(`StrX: ${startX}`);
-  console.log(`T1: ${e.touches[0].pageX}`);
+  // console.log(`StrX: ${startX}`);
+  // console.log(`T1: ${e.touches[0].pageX}`);
   if (!startX) return;
 
   console.log(`ofS2: ${draggableElement.offsetLeft}`);
@@ -52,35 +52,11 @@ draggableElement.addEventListener('touchend', () => {
 });
 
 //
-const deviceWidth = window.innerWidth;
-console.log('the device width is:', deviceWidth);
-// Get the height of the entire document (including content outside the viewport)
-const totalPageHeight = Math.max(
-  document.body.scrollHeight,
-  document.body.offsetHeight,
-  document.documentElement.clientHeight,
-  document.documentElement.scrollHeight,
-  document.documentElement.offsetHeight
-);
-
-console.log('Total page height:', totalPageHeight, 'px');
-
-function setHeight(percentagevalue) {
+const setImageHeight = function () {
+  const contentHeight = document.body.scrollHeight;
   const backgroundSection = document.querySelector('.skycastmain-body');
-  console.log(backgroundSection.clientHeight);
-  const windowHeight = window.innerHeight;
-  console.log(`the height is: ${windowHeight}`);
-  // backgroundSection.style.height = windowHeight + 'px';
-  const newH = totalPageHeight + totalPageHeight * percentagevalue;
+  const newH = contentHeight + 30;
   backgroundSection.style.height = newH + 'px';
-  console.log(backgroundSection.clientHeight);
-}
-
-// Call the function on page load and window resize
-if (deviceWidth <= 700) {
-  window.addEventListener('load', setHeight(0.03));
-  window.addEventListener('resize', setHeight(0.03));
-} else {
-  window.addEventListener('load', setHeight(0.001));
-  window.addEventListener('resize', setHeight(0.001));
-}
+};
+window.addEventListener('load', setImageHeight);
+window.addEventListener('resize', setImageHeight);
