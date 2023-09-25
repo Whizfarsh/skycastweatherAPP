8ad107b583cbac906e17225fd386ef737633ba39
Whizfarsh <oyefarsh@gmail.com>
2023-09-25 03:34:50 +0100

    creeated a separate function to handle the weather requests and datas provided

--- script.js
+++ script.js
@@ -18,6 +18,128 @@ const SCDetails = document.querySelector('.SC--details');
 const SCDailyForecasts = document.querySelector('.SC--daily-forecasts');
 
 //FUNCTIONS
+// **FUNCTION FOR THE WEATHER **
+const weatherAPP = async function (lats, longs) {
+  try {
+    const weatherFatch = await fetch(
+      `https://api.open-meteo.com/v1/gfs?latitude=${lats}&longitude=${longs}&hourly=temperature_2m,relativehumidity_2m,cloudcover,windspeed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,windspeed_10m_max&current_weather=true&windspeed_unit=mph&timezone=auto`
+    );
+    const weatherData = await weatherFatch.json();
+    console.log(weatherData);
+    const currentWeatherTime = weatherData.current_weather.time;
+    const currentWeatherTemp = Math.round(
+      weatherData.current_weather.temperature
+    );
+    let timeNow = currentWeatherTime.split('T');
+    timeNow = timeNow[1];
+    const tempData = weatherData.hourly.temperature_2m;
+    const timeData = weatherData.hourly.time;
+    const himidityData = weatherData.hourly.relativehumidity_2m;
+    const cloudData = weatherData.hourly.cloudcover;
+    const windspeedData = weatherData.hourly.windspeed_10m;
+
+    // updating the current weather status
+    //
+    const getLocation = await fetch(
+      `https://geocode.xyz/${lats},${longs}?geoit=json`
+    );
+    const getLocationData = await getLocation.json();
+    SCWeatherLocation.textContent = getLocationData.city;
+    SCWeatherLocation.style.fontSize = '2.4rem';
+
+    SCCurrentWeather.textContent = `${currentWeatherTemp}°`;
+    // --for the date
+    const date = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const locale = 'en-US';
+
+    const dateNow = new Intl.DateTimeFormat(locale, options).format(date);
+    SCWeatherDate.textContent = dateNow;
+
+    //looping through and creating new array for time and temperature
+    timeData.forEach((theTime, i) => {
+      if (theTime >= currentWeatherTime) {
+        dateRange.push(theTime);
+        tempRange.push(tempData[i]);
+        cloudRange.push(cloudData[i]);
+        humidityRange.push(himidityData[i]);
+        windSpeedRange.push(windspeedData[i]);
+      }
+    });
+    tempRange = tempRange.splice(0, 24);
+    dateRange = dateRange.splice(0, 24);
+    cloudRange = cloudRange.splice(0, 24);
+    humidityRange = humidityRange.splice(0, 24);
+    windSpeedRange = windSpeedRange.splice(0, 24);
+    dateRange.forEach((datesandtime, i) => {
+      const onlyTime = datesandtime.split('T');
+      if (onlyTime[1] === timeNow) {
+        onlyTime[1] = 'Now';
+
+        // insert weather inormations like cloudcover, wind speed, humidity
+        const weatherInfoHtml = `
+      <p class="SC-details"><span>Cloudy</span> ${cloudRange[i]}${weatherData.hourly_units.cloudcover}</p>
+                <p class="SC-details"><span>Humidity</span> ${humidityRange[i]}${weatherData.hourly_units.relativehumidity_2m}</p>
+                <p class="SC-details"><span>WindSpeed</span> ${windSpeedRange[i]}${weatherData.hourly_units.windspeed_10m}</p>
+      `;
+        SCDetails.insertAdjacentHTML('beforebegin', weatherInfoHtml);
+      }
+      const hourlyHtml = `
+      <div class="SC-hourly--TDI">
+                      <span>${onlyTime[1]}</span>
+                      <span>${Math.round(tempRange[i])}°c</span>
+                      <span
+                        ><img
+                          src="icons/sun.png"
+                          class="SC--hourly-weather-icon"
+                          alt=""
+                      /></span>
+                    </div>
+      `;
+      SCHourlyDetails.insertAdjacentHTML('beforeend', hourlyHtml);
+    });
+
+    //for daily forecasts
+    let dailyDateRange = [];
+    // dailyDateRange.push(dateRange.splice(0, 7));
+    // console.log(dailyDateRange);
+
+    const dailyDate = weatherData.daily.time;
+    const dailyTempMinArray = weatherData.daily.temperature_2m_min;
+    const dailyTempMaxArray = weatherData.daily.temperature_2m_max;
+    const dailyWindSpeedMaxArray = weatherData.daily.windspeed_10m_max;
+    dailyDate.forEach((daytime, i) => {
+      const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+
+      const newDate = new Date(daytime);
+      const weekDay = newDate.getDay();
+      const dayOfWeek = date.getDay() === weekDay ? 'Today' : weekDays[weekDay];
+
+      const dailyTemps = Math.floor(
+        (dailyTempMaxArray[i] + dailyTempMinArray[i]) / 2
+      );
+      const dailyForecastHtml = `
+      <div class="SC--daily-forecasts">
+                    <div class="SC--daily-forecast">
+                      <p>${dayOfWeek}</p>
+                      <p><span style="color: #fff">&#177;</span>${dailyTemps}°C</p>
+                      <p>${dailyWindSpeedMaxArray[i]}${weatherData.daily_units.windspeed_10m_max}</p>
+                      <p>
+                        <img
+                          src="icons/sun.png"
+                          class="SC--hourly-weather-icon"
+                          alt=""
+                        />
+                      </p>
+                    </div>
+      `;
+      SCDailyForecasts.insertAdjacentHTML('beforeend', dailyForecastHtml);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+// ******
 // --error function
 const errorMessage = function (msg) {
   scSearchResults.insertAdjacentHTML(
@@ -135,13 +257,22 @@ cityInput.addEventListener('click', function (e) {
   });
 });
 
-//
+//SEARCH RESULT CLICK
 scSearchResults.addEventListener('click', function (e) {
   setOpacity(0);
   cityInput.value = '';
   const resultContainer = e.target.closest('.SC-search-result');
   const lanlongInfo = resultContainer.querySelector('.latnlong').textContent;
   selectedLatAndlong = lanlongInfo.split(',');
+  const clickedLat = Number(selectedLatAndlong[0]);
+  const clickedLong = Number(selectedLatAndlong[1]);
+  document.querySelectorAll('.SC-details').forEach(els => els.remove());
+  document.querySelectorAll('.SC-hourly--TDI').forEach(els => els.remove());
+  // SCDetails.remove();
+  // SCHourlyDetails
+  weatherAPP(clickedLat, clickedLong);
+  console.log(clickedLat);
+  console.log(clickedLong);
 });
 
 // Getting the weather data and working with it.
@@ -157,120 +288,7 @@ const getWeatherInfo = async function () {
     const { latitude: lat, longitude: long } = posUser.coords;
 
     //
-    const weatherFatch = await fetch(
-      `https://api.open-meteo.com/v1/gfs?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,cloudcover,windspeed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,windspeed_10m_max&current_weather=true&windspeed_unit=mph&timezone=auto`
-    );
-    const weatherData = await weatherFatch.json();
-    console.log(weatherData);
-    const currentWeatherTime = weatherData.current_weather.time;
-    const currentWeatherTemp = Math.round(
-      weatherData.current_weather.temperature
-    );
-    let timeNow = currentWeatherTime.split('T');
-    timeNow = timeNow[1];
-    const tempData = weatherData.hourly.temperature_2m;
-    const timeData = weatherData.hourly.time;
-    const himidityData = weatherData.hourly.relativehumidity_2m;
-    const cloudData = weatherData.hourly.cloudcover;
-    const windspeedData = weatherData.hourly.windspeed_10m;
-
-    // updating the current weather status
-    //
-    const getLocation = await fetch(
-      `https://geocode.xyz/${lat},${long}?geoit=json`
-    );
-    const getLocationData = await getLocation.json();
-    SCWeatherLocation.textContent = getLocationData.city;
-    SCWeatherLocation.style.fontSize = '2.4rem';
-
-    SCCurrentWeather.textContent = `${currentWeatherTemp}°`;
-    // --for the date
-    const date = new Date();
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const locale = 'en-US';
-
-    const dateNow = new Intl.DateTimeFormat(locale, options).format(date);
-    SCWeatherDate.textContent = dateNow;
-
-    //looping through and creating new array for time and temperature
-    timeData.forEach((theTime, i) => {
-      if (theTime >= currentWeatherTime) {
-        dateRange.push(theTime);
-        tempRange.push(tempData[i]);
-        cloudRange.push(cloudData[i]);
-        humidityRange.push(himidityData[i]);
-        windSpeedRange.push(windspeedData[i]);
-      }
-    });
-    tempRange = tempRange.splice(0, 24);
-    dateRange = dateRange.splice(0, 24);
-    cloudRange = cloudRange.splice(0, 24);
-    humidityRange = humidityRange.splice(0, 24);
-    windSpeedRange = windSpeedRange.splice(0, 24);
-    dateRange.forEach((datesandtime, i) => {
-      const onlyTime = datesandtime.split('T');
-      if (onlyTime[1] === timeNow) {
-        onlyTime[1] = 'Now';
-
-        // insert weather inormations like cloudcover, wind speed, humidity
-        const weatherInfoHtml = `
-      <p class="SC-details"><span>Cloudy</span> ${cloudRange[i]}${weatherData.hourly_units.cloudcover}</p>
-                <p class="SC-details"><span>Humidity</span> ${humidityRange[i]}${weatherData.hourly_units.relativehumidity_2m}</p>
-                <p class="SC-details"><span>WindSpeed</span> ${windSpeedRange[i]}${weatherData.hourly_units.windspeed_10m}</p>
-      `;
-        SCDetails.insertAdjacentHTML('beforebegin', weatherInfoHtml);
-      }
-      const hourlyHtml = `
-      <div class="SC-hourly--TDI">
-                      <span>${onlyTime[1]}</span>
-                      <span>${Math.round(tempRange[i])}°c</span>
-                      <span
-                        ><img
-                          src="icons/sun.png"
-                          class="SC--hourly-weather-icon"
-                          alt=""
-                      /></span>
-                    </div>
-      `;
-      SCHourlyDetails.insertAdjacentHTML('beforeend', hourlyHtml);
-    });
-
-    //for daily forecasts
-    let dailyDateRange = [];
-    // dailyDateRange.push(dateRange.splice(0, 7));
-    // console.log(dailyDateRange);
-
-    const dailyDate = weatherData.daily.time;
-    const dailyTempMinArray = weatherData.daily.temperature_2m_min;
-    const dailyTempMaxArray = weatherData.daily.temperature_2m_max;
-    const dailyWindSpeedMaxArray = weatherData.daily.windspeed_10m_max;
-    dailyDate.forEach((daytime, i) => {
-      const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
-
-      const newDate = new Date(daytime);
-      const weekDay = newDate.getDay();
-      const dayOfWeek = date.getDay() === weekDay ? 'Today' : weekDays[weekDay];
-
-      const dailyTemps = Math.floor(
-        (dailyTempMaxArray[i] + dailyTempMinArray[i]) / 2
-      );
-      const dailyForecastHtml = `
-      <div class="SC--daily-forecasts">
-                    <div class="SC--daily-forecast">
-                      <p>${dayOfWeek}</p>
-                      <p><span style="color: #fff">&#177;</span>${dailyTemps}°C</p>
-                      <p>${dailyWindSpeedMaxArray[i]}${weatherData.daily_units.windspeed_10m_max}</p>
-                      <p>
-                        <img
-                          src="icons/sun.png"
-                          class="SC--hourly-weather-icon"
-                          alt=""
-                        />
-                      </p>
-                    </div>
-      `;
-      SCDailyForecasts.insertAdjacentHTML('beforeend', dailyForecastHtml);
-    });
+    weatherAPP(lat, long);
   } catch (err) {
     console.log(err);
   }
